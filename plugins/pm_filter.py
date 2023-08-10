21ffaf24545281ea809e38b0b39f88c6b49bd0d9
kallan-suni <141471902+kallan-suni@users.noreply.github.com>
2023-08-10 09:24:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -211,7 +211,7 @@ async def next_page(bot, query):
             ]
             for file in files
         ]
-
+        
      btn.insert(0,
          [
             InlineKeyboardButton(f'🚧 {search} 🚧 ', 'dupe')
